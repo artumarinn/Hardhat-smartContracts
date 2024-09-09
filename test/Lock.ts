c8decbe24480b8c110f06d794b3027d0a0c3ce4e
artumarinn <artumarin.09@gmail.com>
2024-09-09 20:10:28 -0300

    test :Lock Deployment

--- test/Lock.ts
+++ test/Lock.ts
@@ -58,7 +58,7 @@ describe("Lock", function () {
       );
     });
   });
-
+/*
   describe("Withdrawals", function () {
     describe("Validations", function () {
       it("Should revert with the right error if called too soon", async function () {
@@ -124,4 +124,5 @@ describe("Lock", function () {
       });
     });
   });
+  */
 });
