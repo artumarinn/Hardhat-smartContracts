c1427f9c036a2fd80fbb70587acf21a5db853f84
artumarinn <artumarin.09@gmail.com>
2024-09-23 20:16:21 -0300

    run interactSimpleBank: Register/Deposit/Withdraw

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,7 @@ const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.26",
+  solidity: "0.8.24",
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
